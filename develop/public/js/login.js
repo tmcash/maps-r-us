6b2bc9fe215221e83cd17b87fb8c8b8063cc6d16
klozano <klozano17@gmail.com>
2023-03-13 20:33:37 -0400

    added login functions

--- develop/public/js/login.js
+++ develop/public/js/login.js
@@ -22,14 +22,15 @@ const loginFormHandler = async (event) => {
     const signupFormHandler = async (event) => {
         event.preventDefault();
 
-    const username = document.querySelector('#username-signup').value.trim();
+    //const username = document.querySelector('#username-signup').value.trim();
+    const name = document.querySelector('#name-signup').value.trim();
     const email = document.querySelector('#email-signup').value.trim();
     const password = document.querySelector('#password-signup').value.trim();
-
-    if (username && email && password) {
+    //console.log( "login info" , username, email, password);
+    if (name && email && password) {
         const response = await fetch('/api/users', {
         method: 'POST',
-        body: JSON.stringify({ username, email, password }),
+        body: JSON.stringify({ name, email, password }),
         headers: { 'Content-Type': 'application/json' },
     });
 
@@ -38,6 +39,7 @@ const loginFormHandler = async (event) => {
     } else {
         alert('Failed to sign up.');
     }
+    
     }
 };
 
--- develop/server.js
+++ develop/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const session = require('express-session');
 const routes = require('./controllers');
 const exphbs = require('express-handlebars');
-
+const path = require('path');
 
 
 const sequelize = require('./config/connection');
@@ -34,6 +34,7 @@ app.use(session(sess));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
