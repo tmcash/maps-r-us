104205b98a5e913c972721a8e8a189b9eb5c4803
erinsawyer504 <erinsawyer504@gmail.com>
2023-03-16 16:16:15 -0400

    Added GET routes

--- develop/controllers/api/food-routes.js
+++ develop/controllers/api/food-routes.js
@@ -12,4 +12,16 @@ router.post('/', async (req, res) => {
     }
 });
 
+router.get('/city/:cityId', async (req, res) => {
+    try {
+        const food = await Food.findAll({
+        where: { city_id: req.params.cityId }
+    });
+    res.status(200).json(food);
+    } catch (error) {
+    console.error('Error retrieving restaurants:', error);
+    res.status(500).json({ message: 'Internal server error' });
+    }
+});
+
 module.exports = router;
--- develop/controllers/api/hotel-routes.js
+++ develop/controllers/api/hotel-routes.js
@@ -12,4 +12,16 @@ router.post('/', async (req, res) => {
     }
 });
 
+router.get('/city/:cityId', async (req, res) => {
+    try {
+        const hotel = await Hotel.findAll({
+        where: { city_id: req.params.cityId }
+    });
+    res.status(200).json(hotel);
+    } catch (error) {
+    console.error('Error retrieving hotels:', error);
+    res.status(500).json({ message: 'Internal server error' });
+    }
+});
+
 module.exports = router;
