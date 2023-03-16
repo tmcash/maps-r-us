1a621e58ff933414847d3c54f27c4199bb235b54
tmcash <t.cash15@yahoo.com>
2023-03-15 21:11:46 -0400

    Changed city-routes.js code

--- develop/controllers/api/city-routes.js
+++ develop/controllers/api/city-routes.js
@@ -1,15 +1,37 @@
 const router = require('express').Router();
-const { Hotel } = require('../../models');
+const { City } = require('../../models');
 
 router.post('/', async (req, res) => {
-    try {
-        const { name, city } = req.body;
-        const hotel = await Hotel.create({ name, city });
-        res.status(201).json(hotel);
-    } catch (error) {
-        console.error('Error saving hotel:', error);
-        res.status(500).json({ message: 'Internal server error' });
+try {
+    const newCity = await City.create({
+    ...req.body,
+    user_id: req.session.user_id,
+    });
+
+    res.status(200).json(newCity);
+} catch (err) {
+    res.status(400).json(err);
+}
+});
+
+router.delete('/:id', async (req, res) => {
+try {
+    const citytData = awaitCity.destroy({
+    where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+    },
+    });
+
+    if (!citytData) {
+    res.status(404).json({ message: 'No city found with this id!' });
+    return;
     }
+
+    res.status(200).json(citytData);
+} catch (err) {
+    res.status(500).json(err);
+}
 });
 
 module.exports = router;
