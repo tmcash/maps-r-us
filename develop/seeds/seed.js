97c82486987f021d5f0165a768558a61b51de7a1
erinsawyer504 <erinsawyer504@gmail.com>
2023-03-16 21:05:20 -0400

    fixed the seed.js file

--- develop/seeds/seed.js
+++ develop/seeds/seed.js
@@ -1,7 +1,11 @@
 const sequelize = require('../config/connection');
 const { User, Activity, City, Food, Hotel  } = require('../models');
 
-
+const userData = require('./userData.json');
+const activityData = require('./activityData.json');
+const foodData = require('./foodData.json');
+const cityData = require('./cityData.json');
+const hotelData = require('./hotelData.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
@@ -11,6 +15,9 @@ const seedDatabase = async () => {
     returning: true,
   });
 
+  
+
+  
   const activities = await Activity.bulkCreate(activityData);
   const cities = await City.bulkCreate(cityData);
   const foods = await Food.bulkCreate(foodData);
