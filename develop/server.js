2b03400b712a1aef3aa16d62808310842aa4eb90
tmcash <t.cash15@yahoo.com>
2023-03-19 16:44:50 -0400

    Added some js and data.json files

--- develop/server.js
+++ develop/server.js
@@ -1,9 +1,6 @@
 const express = require('express');
 const session = require('express-session');
 const routes = require('./controllers');
-const exphbs = require('express-handlebars');
-const path = require('path');
-
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
@@ -13,12 +10,7 @@ const PORT = process.env.PORT || 3001;
 
 const sess = {
   secret: 'Super secret secret',
-  cookie: {
-    maxAge: 60 * 60 * 1000,
-    httpOnly: true,
-    secure: false,
-    sameSite: 'strict',
-  },
+  cookie: {},
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
@@ -26,15 +18,10 @@ const sess = {
   })
 };
 
-const hbs = exphbs.create({  });
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
-
 app.use(session(sess));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
