f526c3f52bb66bf1c879f610b069da141e856111
erinsawyer504 <erinsawyer504@gmail.com>
2023-03-21 15:55:54 -0400

    Deleted code not being used and added more stlying in css

--- develop/controllers/api/user-routes.js
+++ develop/controllers/api/user-routes.js
@@ -27,26 +27,6 @@ router.post('/signup', async (req, res) => {
     }
   });
 
-
-// router.post('/signup', async (req, res) => {
-//     try {
-//     const dbUserData = await User.create({
-//         name: req.body.name,
-//         email: req.body.email,
-//         password: req.body.password,
-//     });
-
-//     req.session.save(() => {
-//         req.session.loggedIn = true;
-
-//         res.status(200).json(dbUserData);
-//     });
-//     } catch (err) {
-//     console.log(err);
-//     res.status(500).json(err);
-//     }
-// });
-
 // Login
 router.post('/login', (req, res) => {
     User.findOne({
@@ -74,47 +54,6 @@ router.post('/login', (req, res) => {
         });
     });
 });
-
-
-
-
-// router.post('/login', async (req, res) => {
-//     try {
-//     const dbUserData = await User.findOne({
-//         where: {
-//         email: req.body.email,
-//     },
-//     });
-
-//     if (!dbUserData) {
-//         res
-//         .status(400)
-//         .json({ message: 'Incorrect email or password. Please try again!' });
-//     return;
-//     }
-
-//     const validPassword = await dbUserData.checkPassword(req.body.password);
-
-//     if (!validPassword) {
-//     res
-//         .status(400)
-//         .json({ message: 'Incorrect email or password. Please try again!' });
-//     return;
-//     }
-
-//     req.session.save(() => {
-//         req.session.user_id = dbUserData.id;
-//         req.session.loggedIn = true;
-
-//     res
-//         .status(200)
-//         .json({ user: dbUserData, message: 'You are now logged in!' });
-//     });
-//     } catch (err) {
-//     console.log(err);
-//     res.status(500).json(err);
-//     }
-// });
 
 // Logout
 router.post('/logout', (req, res) => {
