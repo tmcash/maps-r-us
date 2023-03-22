c91bdc998810cd3546ab3b4778cea2e69e679c05
erinsawyer504 <erinsawyer504@gmail.com>
2023-03-22 18:30:16 -0400

    got the buttons to work correctly when selecting an activity

--- public/js/index.js
+++ public/js/index.js
@@ -63,80 +63,93 @@ let buttonClickHandler = function (event) {
     } 
 };
 
-let getActivity = function(city, cityLatLong, cityId) {
-    let queryURL =
-      "https://places.ls.hereapi.com/places/v1/discover/explore?&at=" +
-      cityLatLong +
-      "&cat=sights-museums&apiKey=" +
-      APIKey;
-  
-    fetch(queryURL).then(function(response) {
-      if (response.ok) {
-        response.json().then(function(data) {
-          // clear the result grids before populating with new data
-          resultGridAll.innerHTML = "";
-  
-          // add event listener to result grid before generating activity cards
-          resultGridAll.addEventListener("click", function(event) {
-            if (event.target.classList.contains("activity-btn")) {
-              console.log("event.target: ",event.target);
-              console.log("data.results: ", data.results);
-              let index = Array.prototype.indexOf.call(
-                event.target.parentElement.parentElement.children,
-                event.target.parentElement
-              );
-              // get the activity data and save it to the database
-              let activity = {
-                name: data.results.items[index].title,
-                type: data.results.items[index].category.title,
-                address: data.results.items[index].vicinity,
-                city_id: cityId,
-                city: city
-              };
-              console.log("Sending activity data:", JSON.stringify(activity));
-              // post the activity data to the server
-              fetch("./api/activity", {
-                method: "POST",
-                headers: {
-                  "Content-Type": "application/json"
-                },
-                body: JSON.stringify(activity)
-              })
-                .then(response => response.json())
-                .then(data => console.log(data))
-                .catch(error => console.error(error));
-            }
-          });
-  
-          //setting loop to give back the top 10 attractions
-          for (let i = 0; i < 10; i++) {
-            console.log("Title: ", data.results.items[i].title);
-  
-            //adding details from API as text to HTML
-            let activityInfo = `
-              <div class="activity-info">
-                  <img class="activity-icon" src="${data.results.items[i].icon}" alt="activity-icon"></img>
-                  <ul class="activity-misc-info"> 
-                      <li class="title"><b>Name:</b> ${
-                        data.results.items[i].title
-                      }</li>
-                      <li class="type"><b>Type:</b> ${
-                        data.results.items[i].category.title
-                      }</li>
-                      <li class="address"><b>Address:</b> ${
-                        data.results.items[i].vicinity
-                      }</li>
-                      <button class="btn activity-btn bg-teal-800 hover:bg-teal-400 hover:font-bold text-white text-lg">Add to Activity List</button>
-                  </ul>
-              </div>
-            `;
-            // add the activityInfo to the HTML
-            resultGridAll.innerHTML += activityInfo;
-          }
-        });
-      }
-    });
-  };
+
+let getActivity = async function(city, cityLatLong, cityId) {
+
+
+  let queryURL =
+    "https://places.ls.hereapi.com/places/v1/discover/explore?&at=" +
+    cityLatLong +
+    "&cat=sights-museums&apiKey=" +
+    APIKey;
+
+  // define a function to handle the click event
+  function handleAddToActivityListClick(event, data) {
+    if (event.target.classList.contains("activity-btn")) {
+      let index = Array.prototype.indexOf.call(
+        event.target.parentElement.parentElement.children,
+        event.target.parentElement
+      );
+      let eventTargetId = event.target.id;
+      // get the activity data and save it to the database
+      let activity = {
+        name: data.results.items[eventTargetId].title,
+        type: data.results.items[eventTargetId].category.title,
+        address: data.results.items[eventTargetId].vicinity,
+        city_id: cityId,
+        city: city
+    };
+
+
+      console.log("index", index);
+      console.log("Sending activity data:", JSON.stringify(activity));
+      // post the activity data to the server
+      fetch("./api/activity", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(activity)
+      })
+        .then(response => response.json())
+        .then(data => console.log(data))
+        .catch(error => console.error(error));
+    }
+  }
+
+  // remove the event listener before adding it again
+  resultGridAll.removeEventListener("click", handleAddToActivityListClick);
+
+  // add the event listener to the result grid
+  let response = await fetch(queryURL);
+  let data = await response.json();
+
+  resultGridAll.addEventListener("click", event =>
+    handleAddToActivityListClick(event, data)
+  );
+
+  // clear the result grids before populating with new data
+  resultGridAll.innerHTML = "";
+
+  //setting loop to give back the top 10 attractions
+  for (let i = 0; i < 10; i++) {
+    console.log("Title: ", data.results.items[i].title);
+
+    //adding details from API as text to HTML
+    let activityInfo = `
+            <div class="activity-info">
+                <img class="activity-icon" src="${
+                  data.results.items[i].icon
+                }" alt="activity-icon"></img>
+                <ul class="activity-misc-info"> 
+                    <li class="title"><b>Name:</b> ${
+                      data.results.items[i].title
+                    }</li>
+                    <li class="type"><b>Type:</b> ${
+                      data.results.items[i].category.title
+                    }</li>
+                    <li class="address"><b>Address:</b> ${
+                      data.results.items[i].vicinity
+                    }</li>
+                    <button class="btn activity-btn bg-teal-800 hover:bg-teal-400 hover:font-bold text-white text-lg" id=${i}>Add to Activity List</button>
+                </ul>
+            </div>
+          `;
+    // add the activityInfo to the HTML
+    resultGridAll.innerHTML += activityInfo;
+  }
+};
+
   
 //creating event listener for the email button to send an email
 $(document).on('click', '.send-email-btn', function(event) {
@@ -166,81 +179,6 @@ $(document).on('click', '.send-email-btn', function(event) {
   });
   return false;
 });
-
-
-
-
-// let getActivity = function(city, cityId) {
-//     let queryURL = "https://places.ls.hereapi.com/places/v1/discover/explore?&at=" + city + "&cat=sights-museums&apiKey=" + APIKey;
-
-//     fetch(queryURL)
-//     .then(function(response) {
-//         if(response.ok) {
-//             console.log("Activity response", response);
-//             response.json().then(function(data) {
-//                 console.log("Activity data", data);
-
-//                 //setting loop to give back the top 10 attractions
-//                 for(let i=0; i<10; i++){
-//                     console.log("Title: ", data.results.items[i].title);
-      
-                
-//                     //adding details from API as text to HTML
-//                     let activityInfo = `
-//                         <div class="activity-info">
-//                             <img class="activity-icon" src="${data.results.items[i].icon}" alt="activity-icon"></img>
-//                             <ul class="activity-misc-info">
-//                                 <li class="title"><b>Name:</b> ${data.results.items[i].title}</li>
-//                                 <li class="type"><b>Type:</b> ${data.results.items[i].category.title}</li>
-//                                 <li class="address"><b>Address:</b> ${data.results.items[i].vicinity}</li>
-//                                 <button class="btn activity-btn bg-teal-800 hover:bg-teal-400 hover:font-bold text-white text-lg">Add to Activity List</button>
-//                             </ul>
-//                         </div>
-//                     `;
-//                     // add the activityInfo to the HTML
-//                     resultGridAll.innerHTML += activityInfo;
-
-//                     document.addEventListener("click", function(event) {
-//                         if (event.target.classList.contains("activity-btn")) {
-//                           let index = Array.prototype.indexOf.call(
-//                             event.target.parentElement.parentElement.children,
-//                             event.target.parentElement
-//                           );
-//                           // get the activity data and save it to the database
-//                           let activity = {
-//                             name: data.results.items[index].title,
-//                             type: data.results.items[index].category.title,
-//                             address: data.results.items[index].vicinity,
-//                             city_id: cityId,
-//                             to_do: true
-//                           };
-//                           console.log("Sending activity data:", JSON.stringify(activity));
-//                           // post the activity data to the server
-//                           fetch("./api/activity", {
-//                             method: "POST",
-//                             headers: {
-//                               "Content-Type": "application/json"
-//                             },
-//                             body: JSON.stringify(activity)
-//                           })
-//                             .then(response => response.json())
-//                             .then(data => console.log(data))
-//                             .catch(error => console.error(error));
-
-//                         }
-//                       });
-                      
-
-//                 }
-//             });
-//         } else {
-//             console.log('Error: ' + response.statusText);
-//         }
-//     });
-// };
-
-
-
 
 //event listeners
 selectValue.addEventListener("change", buttonSelectHandler);
